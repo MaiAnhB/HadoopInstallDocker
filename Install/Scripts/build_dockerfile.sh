1134fdc129f2c953956ea3460b808c6b9782a65e
MaiAnhB <32357872+MaiAnhB@users.noreply.github.com>
2019-12-17 21:47:31 +0100

    Update build_dockerfile.sh

--- Install/Scripts/build_dockerfile.sh
+++ Install/Scripts/build_dockerfile.sh
@@ -1,1 +1,14 @@
+# add current user to docker group so that it can execute docker commands
+sudo usermod -aG docker $USER
+
+# start docker
+sudo systemctl start docker
+
+# init a docker swarm cluster and listens on localhost
+docker swarm init --advertise-addr 127.0.0.1
+
+# create an overlay network
+docker network create --driver overlay swarm-net
+
+# build dockerfile
 docker build --tag daphnemaianh/hadoop:2.7.4 .
